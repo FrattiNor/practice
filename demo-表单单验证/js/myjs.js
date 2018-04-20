c75a96f825ffd22d4ab957e992c17fe4a611e752
FrattiNor <1270344396@qq.com>
2018-04-20 18:41:33 +0800

    add

--- demo-表单单验证/js/myjs.js
+++ demo-表单单验证/js/myjs.js
@@ -1,43 +1,43 @@
 var submit = document.getElementById("sure");
 var name_var;
 
-// submit.onclick = function(){
-//     name_var = document.getElementById("name").value;
-//     // alert(name_var.length);
-//     if(name_var.length>10||name_var.length<4){
-//         document.getElementById("namep").innerHTML = "请输入的4到10个字符";
-//     }
-//     else{
-//         document.getElementById("namep").innerHTML = "";
-//     }
-// }
+submit.onclick = function(){
+    name_var = document.getElementById("name").value;
+    // alert(name_var.length);
+    if(name_var.length>10||name_var.length<4){
+        document.getElementById("namep").innerHTML = "请输入的4到10个字符";
+    }
+    else{
+        document.getElementById("namep").innerHTML = "";
+    }
+}
 
-// document.getElementById("name").onfocus = function(){
-//     document.getElementById("name").className += " colorr";
-// }
+document.getElementById("name").onfocus = function(){
+    document.getElementById("name").className += " colorr";
+}
 
-// document.getElementById("name").onkeyup = function(){
-//     name_var = document.getElementById("name").value;
-//     // alert(name_var.length);
-//     if(name_var.length>10||name_var.length<4){
-//         document.getElementById("namep").innerHTML = "请输入的4到10个字符";
-//     }
-//     else{
-//         document.getElementById("namep").innerHTML = "";
-//     }
-// }
+document.getElementById("name").onkeyup = function(){
+    name_var = document.getElementById("name").value;
+    // alert(name_var.length);
+    if(name_var.length>10||name_var.length<4){
+        document.getElementById("namep").innerHTML = "请输入的4到10个字符";
+    }
+    else{
+        document.getElementById("namep").innerHTML = "";
+    }
+}
 
-// var oName = document.getElementById("name");
+var oName = document.getElementById("name");
 
-// oName.onkeyup = function(){
-//     name_var = document.getElementById("name").value;
-//     // alert(name_var.length);
-//     if(name_var.length>10||name_var.length<4){
-//         document.getElementById("namep").innerHTML = "请输入的4到10个字符";
-//     }
-//     else{
-//         document.getElementById("namep").innerHTML = "";
-//     }
-// }
+oName.onkeyup = function(){
+    name_var = document.getElementById("name").value;
+    // alert(name_var.length);
+    if(name_var.length>10||name_var.length<4){
+        document.getElementById("namep").innerHTML = "请输入的4到10个字符";
+    }
+    else{
+        document.getElementById("namep").innerHTML = "";
+    }
+}
 
 Object.getOwnPropertyDescriptor( window,'name');
